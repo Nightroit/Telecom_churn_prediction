2a0db43fb31c1a02f48e89c5dda94181a86c6335
Shivam Sharma <shivam@kali>
2024-09-30 09:13:06 -0400

    Data cleaning.

--- analysis.py
+++ analysis.py
@@ -6,8 +6,19 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv('data.csv')
 
 # Data Cleaning
+def data_cleaning(df):
+    for col in df.columns:
+        if len(df[col].unique()) < 100: 
+            df[col] = pd.factorize(df[col])[0]
+    
+    for i in range(len(df["TotalCharges"])):
+        if df["TotalCharges"][i].strip() == "":
+            df["TotalCharges"][i] = 0
+        else: 
+            float(df["TotalCharges"][i])
+            df["TotalCharges"][i] = float(df["TotalCharges"][i])
+    
+    df['MonthlyCharges'] = df['MonthlyCharges'].astype(float) 
 
-for col in df.columns:
-    if len(df[col].unique()) < 100: 
-        df[col] = pd.factorize(df[col])[0]
-
+data_cleaning(df)
+print(type(df["TotalCharges"][0]))
